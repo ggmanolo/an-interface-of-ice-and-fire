af0a8b04fb6d96814bcdf4dac6949a229c6eb750
Manuel Garcia Genta <man.mdp@gmail.com>
2024-10-19 19:48:53 -0300

    member strike name when hover

--- src/components/member/index.tsx
+++ src/components/member/index.tsx
@@ -21,7 +21,11 @@ const Member = async ({ url }: MemberProps) => {
 
   return (
     <div className="border-2 border-gray-700 dark:border-gray-300 p-4 h-64 rounded-lg group cursor-pointer relative flex items-center justify-center overflow-hidden">
-      <h4 className="font-bold uppercase text-xl group-hover:opacity-50 transition-opacity">
+      <h4
+        className={`${
+          isDead && "group-hover:line-through"
+        } font-bold uppercase text-xl group-hover:opacity-50 transition-opacity`}
+      >
         {member.name}
       </h4>
       <div className="transition-transform absolute inset-0 p-4 bg-gray-300/50 dark:bg-gray-700/50 group-hover:translate-y-0 translate-y-full ease-in-out">
